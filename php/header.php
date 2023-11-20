9b8606d90a7e264f22ae593cff44b82c64760458
khni48704 <khni48704@edu.ucl.dk>
2023-11-20 11:10:48 +0100

    php

---
+++ php/header.php
@@ -0,0 +1,10 @@
+<header>
+        <a href="index.html"><img class="logo" src="#" alt="Froggys by night logo"></a>
+        <nav>
+          <ul class="menu">
+            <li><a href="portfolie.html">Events</a></li>
+            <li><a href="#kontakt">Book bord</a></li>
+            <li><a href="#kontakt">Drinks</a></li>
+          </ul>
+        </nav>
+</header>
