13146374630ab9ef71662e69e268bda03dedb72a
khni48704 <khni48704@edu.ucl.dk>
2023-11-21 12:47:55 +0100

    test med php

---
+++ php/footer.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Froggys by night</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;700;800;900&family=Karla:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap'rel="stylesheet" );
+    </style>
+</head>
+
+<footer>
+    <div class="footer-container">
+        <div> <img src="img/frø.png" alt=""> 
+        </div>
+        <div>
+            <h2>Åbningstider</h2><br>
+            <p>Fredag kl. 22-04</p>
+            <p>Lørdag kl. 22-04</p>
+            <br>
+            <br>
+            <p>Aldersgrænse: 18+</p>
+            <p>CVR-nummer: 32438660</p>
+        </div>
+        <div>
+            <h2>Find os</h2><br>
+            <p>Froggys By Night</p>
+            <p>Odense</p>
+            <br>
+            <p>+4565907447</p>
+            <br>
+            <p>froggys@froggyscafe.dk</p>
+        </div>
+    </div>
+
+</footer>
