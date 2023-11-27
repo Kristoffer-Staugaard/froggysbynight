5afbe73e5b0e8dcc86ec56971f9361fb261236e9
khni48704 <hjerno.kasper@gmail.com>
2023-11-27 18:26:10 +0100

    Kaspers firebase/postman med js til drink siden

--- JavaScript/drinkskort.js
+++ JavaScript/drinkskort.js
@@ -1,24 +1,40 @@
-let drinksarray = [
-    ["img/margarita.png", "Margarita", "105 kr,-"],
-    ["img/strawberry.png", "Strawberry daiquiri", "85 kr,-"], 
-    ["img/brumble.png", "Brumble", "130 kr,-"],
-    ["img/martini.png", "Martini", "105 kr,-"],
-    ["img/pinacolada.png", "Pinacolada", "85 kr,-"],
-    ["img/mojito.png", "Mojito", "105 kr,-"],
-    ["img/high-c.png", "High-C", "110 kr,-"],
-    ["img/moscow-mule.png", "Moscow Mule", "90 kr,-"],
-    ["img/aperol-spritz.png", "Aperol Spritz", "105 kr,-"],
-    ["img/dark-and-stormy.png", "Dark And Stormy", "90 kr,-"],
-    ["img/espresso-martini.png", "Espresso Martini", "120 kr,-"],
-    ["img/pornstar-martini.png", "Pornstar martini", "105 kr,-"],
-   ];
+const froggysdrinksMap = {};
+fetch('https://froggy-e3fac-default-rtdb.europe-west1.firebasedatabase.app/drinks.json')
+    .then(response => response.json())
+    .then(data => {
+        
+        let contentContainer = document.getElementById("contentContainer");
+        contentContainer.id = "overskrift-og-tekst";
+        contentContainer.classList.add("drinkskort")
 
-   let select_element = document.getElementById("overskrift-og-tekst");
-   drinksarray.forEach(drink => {
-       select_element.innerHTML += "<div class='drinksection'><p class='text-styling-H3'>" + drink[1] + "</p><p class='text-styling-pris'>" + drink[2] + "</p><img class='drinkskortbilleder' src='" + drink[0] + "'></div>";
-  });
-   
-  // for (let i = 0; i < drinksarray.length; i++) {
-//       let drink = drinksarray[i];
-  //     select_element.innerHTML += "<div class='drinksection'><p class='text-styling-H3'>" + drink[1] + "</p><p class='text-styling-pris'>" + drink[2] + "</p><img class='drinkskortbilleder' src='" + drink[0] + "'></div>";
+        for (let key in data) {
+            let dataarray = data[key];
+
+            
+            let drinkElement = document.createElement("div");
+            drinkElement.classList.add("drinksection");
       
+            //drinkElement.textContent = `${dataarray.name} ${dataarray.price} kr. `;
+
+            let nameElement = document.createElement("p");
+            nameElement.classList.add("text-styling-H3");
+            nameElement.textContent = dataarray.name;
+
+            
+            let priceElement = document.createElement("p");
+            priceElement.classList.add("text-styling-pris");
+            priceElement.textContent = `${dataarray.price} kr.`;
+
+            
+            let imageElement = document.createElement("img")
+            imageElement.src = dataarray.image;
+            imageElement.classList.add("drinkskortbilleder");
+
+            drinkElement.appendChild(nameElement);
+            drinkElement.appendChild(priceElement);
+            drinkElement.appendChild(imageElement);
+             
+            contentContainer.appendChild(drinkElement);
+        }
+    })
+    .catch(error => console.error('Fejl:', error));
