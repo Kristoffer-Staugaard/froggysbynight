1b209b431fd2e375dce4292a9d8b7fbaf6153a53
Kristoffer-Staugaard <kristofferstaugaard@gmail.com>
2023-11-27 19:21:21 +0100

    Create events.js

---
+++ javascript/events.js
@@ -0,0 +1,7 @@
+const froggysEventsMap = {};
+fetch('https://froggy-e3fac-default-rtdb.europe-west1.firebasedatabase.app/events.json')
+    .then(response => response.json())
+    .then(data => {
+
+
+    })
