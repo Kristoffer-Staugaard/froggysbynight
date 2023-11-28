cf32678219469266061ced3837ca2b0ffd8abcd8
Kristoffer-Staugaard <kristofferstaugaard@gmail.com>
2023-11-28 09:54:51 +0100

    events js

--- javascript/events.js
+++ javascript/events.js
@@ -4,4 +4,40 @@ fetch('https://froggy-e3fac-default-rtdb.europe-west1.firebasedatabase.app/event
     .then(data => {
 
 
+        let eventsContainer = document.getElementById("eventsContainer")
+        eventsContainer.id = "events-title";
+        eventsContainer.classList.add("events")
+
+        for (let key in data) {
+            let dataarray = data[key];
+
+        let eventsElement = document.createElement("div");
+        eventsElement.classList.add("events-cards");
+
+        // console.log( `${dataarray.date} ${dataarray.img}`);
+
+        let titleElement = document.createElement("p");
+            titleElement.classList.add("events-title");
+            titleElement.textContent = dataarray.title;
+
+        let dateElement = document.createElement("p");
+            dateElement.classList.add("events-date");
+            dateElement.textContent = dataarray.date;
+
+        let descriptionElement = document.createElement("p");
+            descriptionElement.classList.add("events-description");
+            descriptionElement.textContent = dataarray.description;
+
+        let imgElement = document.createElement("img");
+            imgElement.classList.add("events-img");
+            imgElement.src = dataarray.img;
+
+        eventsElement.appendChild(titleElement);
+        eventsElement.appendChild(dateElement);
+        eventsElement.appendChild(descriptionElement);
+        eventsElement.appendChild(imgElement);
+
+        eventsContainer.appendChild(eventsElement);
+        }
     })
+    .catch(error => console.error('Fejl:', error));
