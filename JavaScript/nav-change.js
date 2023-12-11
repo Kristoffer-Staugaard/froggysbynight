947b9f3b274898078daf20a0979649c936f63ea1
Kristoffer-Staugaard <kristofferstaugaard@gmail.com>
2023-12-11 09:51:28 +0100

    rettelser

--- JavaScript/nav-change.js
+++ JavaScript/nav-change.js
@@ -2,7 +2,6 @@ window.addEventListener("scroll", function () {
     var navbar = document.getElementById("navbar");
     var header = document.querySelector("header");
 
-    // Check if the scroll position is below the header
     if (window.scrollY > header.offsetHeight) {
         navbar.classList.add("scrolled");
     } else {
